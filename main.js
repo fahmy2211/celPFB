35ee7dd9aad7cb53503ef8d05a80e8abb84482e1
fahmy2211 <116193981+fahmy2211@users.noreply.github.com>
2023-04-19 08:53:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -44,6 +44,6 @@ app.post('/', (req, res) => {
   }
 });
 
-app.listen(4010, () => {
-  console.log('Server is listening on port 4010');
+app.listen(6070, () => {
+  console.log('Server is listening on port 6070');
 });
